d4514799b10684fe8cc9d7cb8e9733e3cce20c85
Dickens Zihua Fan <dickensdjj@gmail.com>
2018-09-24 16:08:50 +1000

    User can create reply

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -68,7 +68,8 @@
         {{-- replies list --}}
         <div class="panel panel-default topic-reply">
             <div class="panel-body">
-                @include('topics._reply_box', ['topic' => $topic])
+                @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+               
                 @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
             </div>
         </div>
